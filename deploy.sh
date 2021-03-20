ae2438ccfd043b3c926ae558a8e79a2c5a0c1534
xiaoyou66 <xiaoyou2333@foxmail.com>
2021-03-20 20:21:03 +0800

    add: 添加cname

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github
-#echo 'interview.xiaoyou66.com' > CNAME
+echo 'interview.xiaoyou66.com' > CNAME
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:xiaoyou66/interview.git
